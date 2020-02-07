febf07c327efc0f0f5a974aedfb00635914a980a
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2020-02-07 21:40:07 +0900

    add reconnect

--- pcounter-store.go
+++ pcounter-store.go
@@ -31,6 +31,7 @@ var (
     version = "0.01"
     baseDir = "store"
     dataDir string
+    sxServerAddress string
     ds DataStore
 )
 
@@ -108,10 +109,35 @@ func supplyPCounterCallback(clt *sxutil.SXServiceClient, sp *api.Supply) {
     }
 }
 
+func reconnectClient(client *sxutil.SXServiceClient) {
+	mu.Lock()
+	if client.Client != nil {
+		client.Client = nil
+		log.Printf("Client reset \n")
+	}
+	mu.Unlock()
+	time.Sleep(5 * time.Second) // wait 5 seconds to reconnect
+	mu.Lock()
+	if client.Client == nil {
+		newClt := sxutil.GrpcConnectServer(sxServerAddress)
+		if newClt != nil {
+			log.Printf("Reconnect server [%s]\n", sxServerAddress)
+			client.Client = newClt
+		}
+	} else { // someone may connect!
+		log.Printf("Use reconnected server\n", sxServerAddress)
+	}
+	mu.Unlock()
+}
+
+
 func subscribePCounterSupply(client *sxutil.SXServiceClient) {
     ctx := context.Background() //
-    client.SubscribeSupply(ctx, supplyPCounterCallback)
-    log.Fatal("Error on subscribe")
+    for { // make it continuously working..
+    	client.SubscribeSupply(ctx, supplyPCounterCallback)
+    	log.Print("Error on subscribe")
+	reconnectClient(client)
+    }
 }
 
 
@@ -122,19 +148,20 @@ func main() {
 
     channelTypes := []uint32{pbase.PEOPLE_COUNTER_SVC}
 
-    srv, rerr := sxutil.RegisterNode(*nodesrv, "PCouterStore", channelTypes, nil)
+    var rerr error
+    sxServerAddress, rerr = sxutil.RegisterNode(*nodesrv, "PCouterStore", channelTypes, nil)
 
     if rerr != nil {
         log.Fatal("Can't register node:",rerr)
 	}
 	if *local != ""{// quick hack for AWS local network
-		srv = *local
+		sxServerAddress = *local
 	}
-    log.Printf("Connecting SynerexServer at [%s]", srv)
+    log.Printf("Connecting SynerexServer at [%s]", sxServerAddress)
 
     wg := sync.WaitGroup{} // for syncing other goroutines
 
-    client := sxutil.GrpcConnectServer(srv)
+    client := sxutil.GrpcConnectServer(sxServerAddress)
 
     if client == nil {
         log.Fatal("Can't connect Synerex Server")
